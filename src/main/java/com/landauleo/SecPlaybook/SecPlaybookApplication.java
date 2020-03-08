2bc402dc436497fbfa0af45dfeb3a93348f058ab
ruanaaf <Anna.G@RAIFFEISEN.ru>
2020-03-08 19:33:06 +0300

    initial commit

---
+++ src/main/java/com/landauleo/SecPlaybook/SecPlaybookApplication.java
@@ -0,0 +1,13 @@
+package com.landauleo.SecPlaybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecPlaybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecPlaybookApplication.class, args);
+	}
+
+}
